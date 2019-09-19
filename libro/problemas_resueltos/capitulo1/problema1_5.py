08201672a869a1c07ec488b25e3d5fc5529dc6c9
ArturoBarrios9000 <arturobarrios9000@gmail.com>
2019-09-19 17:32:00 -0500

    correccion del problema1.5

--- libro/problemas_resueltos/capitulo1/problema1_5.py
+++ libro/problemas_resueltos/capitulo1/problema1_5.py
@@ -1,6 +1,6 @@
 RAD=float(input("Ingrese el radio del cilindro:"))
 ALT=float(input("Ingrese la altura del cilindro:"))
-VOL=3.141592*RAD*ALT
+VOL=3.141592*(RAD**2)*ALT
 ARE=2*3.141592*RAD*ALT
 print(f"El volumen del cilindro es {VOL} y el area es {ARE}U")
 
