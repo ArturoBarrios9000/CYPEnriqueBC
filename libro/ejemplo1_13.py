b7a3489a6a44c441a85ebd5ead79410b6ed48817
ArturoBarrios9000 <arturobarrios9000@gmail.com>
2019-09-21 19:23:48 -0500

    ejemplos1.13-1.15

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,8 @@
+MAT=int(input("Introdusca no. de matricula:"))
+CAL1=float(input("Introduca primera calificacion:"))
+CAL2=float(input("Introdusca segunda calificacion:"))
+CAL3=float(input("Introdusca tercera calificacion"))
+CAL4=float(input("Introdusca cuarta calificacion:"))
+CAL5=float(input("Introdusca quinta calificacion:"))
+PRO=(CAL1+CAL2+CAL3+CAL4+CAL5)/5
+print(f"El promedio del alumno con matricula: {MAT} tiene un promedio de {PRO}")
---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,6 @@
+BAS = int(input("Dame la base:"))
+ALT = int(input("Dame la altura:"))
+SUP = BAS * ALT
+PER = 2 * (BAS + ALT)
+print(f"La superficie es: { SUP }")
+print(f"El perimetro es: { PER }")
